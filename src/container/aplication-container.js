d4ba8c29b2f31a8419edf63e5835ccc48a82fef1
Cesar Trevino <cesar.trevino.garcia@gmail.com>
2022-12-10 12:32:01 -0500

    add component test

--- src/container/aplication-container.js
+++ src/container/aplication-container.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react'
 import '/src/css/test-component.css'
 import TestComponent from '/src/component/test-component'
 
+const PAGES = [
+
+    {
+        link: "TestComponent"
+    }
+]
+
+const URL_BD = "https://filessave-73b1a-default-rtdb.firebaseio.com/nom"
+
 
 class ApplicationContainer extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            valueState: 'value'
+            page: 'TestComponent'
         }
 
 
@@ -17,20 +26,32 @@ class ApplicationContainer extends Component {
     }
 
     unFunction(event) {
-        console.log("bind function, se puede enviar en un componente");
+        console.log("bind function, se puede enviar en un componente", this.state.page);
     }
 
     componentDidMount() {
 
         console.log("ApplicationContainer did mount");
 
+        // fetch(URL_BD)
+        //     .then(response => response.json())
+        //     .then(response => {
+        //         console.log("Respuesta: ", response);
+        //     })
+
     }
+
+
 
     render() {
         return (
             <div className='app-container'>
                 <h1>App Container</h1>
-                <TestComponent param1={"Test component"} param2={"este es un componente de prueba"}/>
+                <TestComponent
+                    param1={"Test component"}
+                    param2={"este es un componente de prueba"} >
+                </TestComponent>
+                <button onClick={this.unFunction}>Check</button>
             </div>
         )
     }
